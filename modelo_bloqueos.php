dd7213bc2dde47d34ce4280bd8d2ae8fd5176e3a
JuanpaBorg <juanpaborg8@live.com>
2018-10-11 21:41:56 -0500

    Update modelo_bloqueos.php

--- modelo_bloqueos.php
+++ modelo_bloqueos.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
  * no esta completo el modelo ya que no se sabe que necesite exactamente el
  * controlador de bloqueos
  * ademas se debera setear en env la base que se va a usar donde se va a usar
+ * Juanpa Boada
  */
 
 class modelo_bloqueos extends Model
