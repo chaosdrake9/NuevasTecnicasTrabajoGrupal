d16fecb62af79b0a8e59c96ba9c33120e2ff982c
JuanpaBorg <juanpaborg8@live.com>
2018-10-11 21:41:15 -0500

    Add files via upload

---
+++ modelo_bloqueos.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/*
+ * no esta completo el modelo ya que no se sabe que necesite exactamente el
+ * controlador de bloqueos
+ * ademas se debera setear en env la base que se va a usar donde se va a usar
+ */
+
+class modelo_bloqueos extends Model
+{
+    protected $table = 'bloqueos';
+    public $timestamp = false;
+
+    protected $fillabe = ['target', 'code', 'expdays'];
+}
